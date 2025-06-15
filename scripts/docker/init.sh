e33251e5d938da351008420c513b0143a78b4c8f
Taylor Callsen <gritto2008@gmail.com>
2025-06-15 12:23:48 -0700

    updating docker init script with comments

--- scripts/docker/init.sh
+++ scripts/docker/init.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
+# note: commands designed for Ubuntu/Debian Linux docker host; do not work on Mac OSX
+
+# note: this script must be run as root
+
 docker volume create video-conversion-rest-api-workdir
 mkdir /var/lib/docker/volumes/video-conversion-rest-api-workdir/_data/source
 mkdir /var/lib/docker/volumes/video-conversion-rest-api-workdir/_data/converted
+
+# note: confirm permissions set in these commands are ok!!
 
 chmod -R 777 /var/lib/docker/volumes/video-conversion-rest-api-workdir/_data
 chmod 666 /var/run/docker.sock
